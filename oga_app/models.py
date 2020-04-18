83819ba8b79ba7f8007fc048cc9ad32bac46ef3f
Ghada6336 <ghadafahmie6336@gmail.com>
2020-04-18 19:36:50 +0300

    added order and orderd item

--- oga_app/models.py
+++ oga_app/models.py
@@ -34,22 +34,42 @@ class Item(models.Model):
 
 
 class Profile(models.Model):
-	GENDER = (
-		("F", "Female"),
-		("M", "Male")
-	)
-	user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-	phone = models.PositiveIntegerField(null=True)
-	gender = models.CharField(choices=GENDER, max_length=2, null=True)
-	age = models.PositiveIntegerField(null=True)
-	image = models.ImageField(null=True)
+    GENDER = (
+        ("F", "Female"),
+        ("M", "Male")
+    )
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    phone = models.PositiveIntegerField(null=True)
+    gender = models.CharField(choices=GENDER, max_length=2, null=True)
+    age = models.PositiveIntegerField(null=True)
+    image = models.ImageField(null=True)
 
+    def __str__(self):
+        return self.user.username
 
-	def __str__(self):
-		return self.user.username
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user= instance)
+        Profile.objects.create(user=instance)
 
+# check
+
+
+class Order(models.Model):
+    user_order = models.ForeignKey(
+        User, on_delete=models.CASCADE,  related_name="user_order")
+    is_order = models.BooleanField(null=True)
+    items = models.ManyToManyField(Item)
+
+    def __str__(self):
+        return self.user_order.username
+
+
+class OrderItem(models.Model):
+    order_owner = models.ForeignKey(
+        "Order", on_delete=models.CASCADE,  related_name="order_owner")
+    order_item = models.ForeignKey(
+        Item, on_delete=models.CASCADE,  related_name="order_item")
+    quantity_num = models.PositiveIntegerField(null=True)
